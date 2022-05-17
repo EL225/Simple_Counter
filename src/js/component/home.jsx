678f651a023ff32518aad2a275e2e7e35cbfc35a
EL225 <ericleonmora99@gmail.com>
2022-05-17 02:50:33 +0000

    finalizado

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,60 @@
 import React from "react";
 
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
-const Home = () => {
+const reloj = {
+	display: "flex",
+	color: "white",
+};
+const num = {
+	padding: "10px",
+	width: "50px",
+	background: "#2E2E2E ",
+	border: "1px solid #848484",
+	margin: "5px",
+	textAlign: "center",
+	borderRadius: 6,
+	alignItems: "center",
+};
+const clock = {
+	paddingTop: "20px",
+	width: "50px",
+	background: "#2E2E2E ",
+	border: "1px solid #848484",
+	margin: "5px",
+	textAlign: "center",
+	borderRadius: 6,
+	alignItems: "center",
+};
+const Home = (props) => {
 	return (
-		<div>
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div style={{ background: "black" }}>
+			<div className="d-flex justify-content-center" style={reloj}>
+				<div style={clock}>
+					<i
+						style={{ fontSize: "35px" }}
+						className="fas fa-clock"></i>
+				</div>
+				<div style={num}>
+					<h1>{props.num6}</h1>
+				</div>
+				<div style={num}>
+					<h1>{props.num5}</h1>
+				</div>
+				<div style={num}>
+					<h1>{props.num4}</h1>
+				</div>
+				<div style={num}>
+					<h1>{props.num3}</h1>
+				</div>
+				<div style={num}>
+					<h1>{props.num2}</h1>
+				</div>
+				<div style={num}>
+					<h1>{props.num1}</h1>
+				</div>
+			</div>
 		</div>
 	);
 };
--- src/js/index.js
+++ src/js/index.js
@@ -7,6 +7,26 @@ import "../styles/index.css";
 
 //import your own components
 import Home from "./component/home.jsx";
-
+let counter = 0;
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+setInterval(() => {
+	let num1 = Math.floor(counter / 1) % 10;
+	let num2 = Math.floor(counter / 10) % 10;
+	let num3 = Math.floor(counter / 100) % 10;
+	let num4 = Math.floor(counter / 1000) % 10;
+	let num5 = Math.floor(counter / 10000) % 10;
+	let num6 = Math.floor(counter / 100000) % 10;
+
+	ReactDOM.render(
+		<Home
+			num6={num6}
+			num5={num5}
+			num4={num4}
+			num3={num3}
+			num2={num2}
+			num1={num1}
+		/>,
+		document.querySelector("#app")
+	);
+	counter++;
+}, 1000);
